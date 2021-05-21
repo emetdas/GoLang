f2034e3f23fd0de25d3a89aefca5fe98b90c1de3
emetdas <emet@b2bleadstaff.com>
2021-05-21 18:36:24 +0600

    golang

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+// Go Variable
+
+// strings ==> string
+// number ==> int
+func main()  {
+	// strings-start 
+		// var nameOne string = "Emet"
+		// var Number float32 = 20
+	// strings-end
+	// Number-start 
+		var inTerger int = 3020
+	// Number-end 
+	fmt.Println(inTerger + 20)
+}
+
+
+
